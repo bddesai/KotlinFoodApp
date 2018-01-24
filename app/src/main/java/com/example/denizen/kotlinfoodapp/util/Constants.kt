1d8210043f83f123cfab7d7640cf8b593c455bf7
Bhavin <bhavin.desai90@gmail.com>
2018-01-24 13:19:22 -0800

    Add OkHttp for porting interceptor to add request headers

--- app/src/main/java/com/example/denizen/kotlinfoodapp/util/Constants.kt
+++ app/src/main/java/com/example/denizen/kotlinfoodapp/util/Constants.kt
@@ -8,6 +8,7 @@ package com.example.denizen.kotlinfoodapp.util
 class Constants{
     companion object {
         val BASE_URL = "https://api.yelp.com/v3/businesses/"
+        val AUTH_HEADER = "Bearer nkynG8v_g-FyTFICuVkHYX0SNDPuKhiZ_1SgzcBMS09ZdxuNNuleH7Ck6QCXn_zzjvNunF_RlKvbbOWlEgIHQdRRNme2Woq3nBdHUYBpBkWw8XFeQ0Gyha2kDC9dWnYx"
 
         fun isReallyNull(string: String): Boolean{
             if(string.isNullOrEmpty() || string.isNullOrBlank()){
